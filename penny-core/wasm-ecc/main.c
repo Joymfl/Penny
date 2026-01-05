9ab6377fa7ec9b0d805f70b14ddea3e618f9a561
Joymfl <jmjoymallik@gmail.com>
2026-01-05 19:43:14 +0530

    Don't need to target emscripten
    Was working off a bad assumption that emscripten is the best way to
    support browser compatibility. Switching to wasm32 target and rust for
    complex lifetime management support

--- penny-core/wasm-ecc/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Hello from wasm-ecc (EMScripten)!\n");
-	return 42;
-}
--- penny-core/wasm-wasi/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	puts("Hello from wasm-wasi");
-	return 123;
-}
